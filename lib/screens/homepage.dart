18ddccfaa31e477ecb151343ab591c391307dead
unknown <Nirajdhiwar.pune@gmail.com>
2021-06-30 21:52:36 +0530

    .add

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -24,7 +24,7 @@ class Homepage extends StatefulWidget {
       name: "Day3-Shoulder",
     ),
     Category(
-      imagUrl: "assets/images/biceps.png",
+      imagUrl: "assets/images/bicep.png",
       name: "Day4-Biceps",
     ),
     Category(
